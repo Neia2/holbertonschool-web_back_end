2a3e1d70df62bfd85cab2cc4d3e4cd66dd612adc
Neia2 <nenaslife@gmail.com>
2024-07-28 03:36:52 +0200

    updating code

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
 // 1-get_list_student_ids.js
 export default function getListStudentIds(array) {
-	if (!Array.isArray(array)) {
-	  return [];
-	}
-	return array.map(student => student.id);
- }
- 
+  if (!Array.isArray(array)) {
+    return [];
+  }
+  return array.map((student) => student.id);
+}
---
+++ ES6_data_manipulation/eslint.config.js
@@ -0,0 +1,22 @@
+import { ESLint } from 'eslint';
+
+export default [
+  {
+    ignores: ['node_modules/**'],
+  },
+  {
+    files: ['**/*.js', '**/*.mjs'],
+    languageOptions: {
+      sourceType: 'module',
+    },
+    rules: {
+      'no-tabs': 'error',
+      'indent': ['error', 2],
+      'no-mixed-spaces-and-tabs': 'error',
+      'arrow-parens': ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'no-multiple-empty-lines': ['error', { max: 0 }],
+      'eol-last': ['error', 'always'],
+    },
+  },
+];
