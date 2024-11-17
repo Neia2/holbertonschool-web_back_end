15198967abb830a4d97925dc3e5fc00be6291668
Neia2 <7276@holbertonstudents.com>
2024-11-17 16:17:29 +0100

    fixing t0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,16 +1,14 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
-  
+  const task = 'I prefer const when I can.'; // Use `const` for immutable values
+  return task; // The variable `task` is used here
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let'; // Use `let` for values that can be reassigned
+  combination += getLast(); // Append the value returned by `getLast()`
+  return combination; // Return the updated `combination`
+}
