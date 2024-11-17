ce7372d9094d36ad484af7624ce1cdfa6ef973ad
Neia2 <7276@holbertonstudents.com>
2024-11-17 23:00:30 +0100

    updating t9

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,5 +1,4 @@
-// Define the HolbertonClass class
-export class HolbertonClass {
+class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -14,12 +13,7 @@ export class HolbertonClass {
   }
 }
 
-// Create instances of HolbertonClass
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-// Define the StudentHolberton class
-export class StudentHolberton {
+class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -39,12 +33,15 @@ export class StudentHolberton {
   }
 }
 
-// Create instances of StudentHolberton
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export the list of students
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
