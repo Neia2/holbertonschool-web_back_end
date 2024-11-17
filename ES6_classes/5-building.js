353a540e97f4b0def0cfbd4438aa2d661091b61d
Neia2 <7276@holbertonstudents.com>
2024-11-17 23:09:31 +0100

    updating 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (new.target === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    // Validate the type of sqft
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
     }
+    this._sqft = sqft;
   }
 
+  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
+  // Abstract method
+  /* eslint-disable-next-line class-methods-use-this */
   evacuationWarningMessage() {
-    // Reference `this` to satisfy ESLint
-    if (this instanceof Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
