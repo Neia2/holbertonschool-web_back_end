b6e40401b518f5eb3c43d886c7d4f94fed428c51
Neia2 <7276@holbertonstudents.com>
2024-12-01 15:33:47 +0100

    Restore ES6_promise directory and files

--- ES6_basic/10-loops.js
+++
@@ -1,7 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const result = [];
-  for (const [index, value] of array.entries()) {
-    result[index] = appendString + value;
-  }
-  return result;
-}
--- ES6_data_manipulation/8-clean_set.js
+++
@@ -1,7 +0,0 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') return '';
-  return Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
-}
