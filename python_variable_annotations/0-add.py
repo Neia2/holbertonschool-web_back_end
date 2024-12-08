1283eef3a4daf6a937cd7c9e0bc2b7f54fe83331
Neia2 <7276@holbertonstudents.com>
2024-12-08 17:46:37 +0100

    docstring t0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -6,6 +6,7 @@ The function 'add' takes two parameters, both integers or floats,
 and returns their sum as a result.
 """
 
+
 def add(a: float, b: float) -> float:
     """Adds two numbers and returns the sum."""
     return a + b
