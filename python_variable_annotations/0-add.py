087218405e0ecc6a5839f2d037568740576aec8b
Neia2 <7276@holbertonstudents.com>
2024-12-08 17:29:49 +0100

    correcting docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
+"""The function 'add' takes two parameters, both integers or floats"""
 def add(a: float, b: float) -> float:
+    """Adds two numbers and returns the sum."""
     return a + b
