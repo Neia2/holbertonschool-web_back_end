f8153119cbfaad9eea4f9100b778e3dfe15579bf
Neia2 <7276@holbertonstudents.com>
2024-12-08 17:18:37 +0100

    t0-t9

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+""" Type-annotated function to_str """
+def to_str(n: float) -> str:
+    return str(n)
---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Type-annotated function to_kv """
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple with a string and the square of a value as a float."""
+    return (k, float(v ** 2))
