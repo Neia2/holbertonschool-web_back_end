6f75147db9bd880d47546c637e93401344dcc871
Neia2 <7276@holbertonstudents.com>
2024-12-08 17:42:19 +0100

    correcting t0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-"""The function 'add' takes two parameters, both integers or floats"""
+"""
+This module contains a function to add two numbers.
+
+The function 'add' takes two parameters, both integers or floats,
+and returns their sum as a result.
+"""
+
 def add(a: float, b: float) -> float:
     """Adds two numbers and returns the sum."""
     return a + b
