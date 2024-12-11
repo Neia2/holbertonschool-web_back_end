49b580c9e99ed6b43d1a565b45bf8a31ed94872d
Neia2 <7276@holbertonstudents.com>
2024-12-12 00:19:12 +0100

    0-async_gen pep 8

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,6 +5,7 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     An asynchronous generator that yields 10 random numbers between 0 and 10.
